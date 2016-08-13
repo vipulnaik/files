36477a4d884cf9887d1a5e0ded508e9b36c019b8
Vipul Naik <vipulnaik1@gmail.com>
2016-08-13 15:35:08 +0000

    Created initial files and cleared them

---
+++ olympiads/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<?
+$curr_menu="Math Olympiad";
+include ("../head.inc");
+echo("<div id='menu'>");
+include("$base_dir/menu.inc");
+echo("<div id='main'>");
+include("home.inc");
+?>
---
+++ undergraduate/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<?
+$base_dir="..";
+$curr_menu="Academic Life";
+include ("$base_dir/head.inc");
+echo("<div id='menu'>");
+include("$base_dir/menu.inc");
+echo("<div id='main'>");
+include("home.inc");
+?>
